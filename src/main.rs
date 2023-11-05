9a3831ece4062aa0fe8428d397c0011fd17d499c
Kanji Tanaka <ktanaka@ichain.co.jp>
2023-11-05 14:05:40 +0900

    Fix trace layer

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,11 @@ async fn main() {
 
     let app = root::router(shared_state).layer(
         TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().include_headers(true))
+            .make_span_with(
+                DefaultMakeSpan::new()
+                    .level(Level::INFO)
+                    .include_headers(true),
+            )
             .on_request(DefaultOnRequest::new().level(Level::INFO))
             .on_response(
                 DefaultOnResponse::new()
