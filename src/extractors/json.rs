094b8fd396560bd99723a51230cdb18c364390d1
Kanji Tanaka <ktanaka@ichain.co.jp>
2023-11-08 16:37:12 +0900

    Refactor json extractor

--- src/extractors/json.rs
+++ src/extractors/json.rs
@@ -1,8 +1,11 @@
 use crate::error::HttpError;
 
-use axum::{async_trait, extract::FromRequest, http::Request, RequestExt};
+use axum::{
+    async_trait,
+    extract::{rejection::JsonRejection, FromRequest},
+    http::Request,
+};
 use serde::Deserialize;
-use std::fmt;
 use validator::Validate;
 
 pub struct Json<J>(pub J);
@@ -13,24 +16,22 @@ where
     B: Send + 'static,
     S: Send + Sync,
     J: FromValidate + 'static,
-    <J as FromValidate>::Validatable: Validate + for<'de> Deserialize<'de>,
-    axum::Json<<J as FromValidate>::Validatable>: FromRequest<(), B>,
-    <axum::Json<<J as FromValidate>::Validatable> as FromRequest<(), B>>::Rejection: fmt::Display,
+    axum::Json<<J as FromValidate>::Validatable>: FromRequest<S, B, Rejection = JsonRejection>,
 {
     type Rejection = HttpError;
 
     async fn from_request(req: Request<B>, _state: &S) -> Result<Self, Self::Rejection> {
-        let axum::Json(req) = req
-            .extract::<axum::Json<<J as FromValidate>::Validatable>, _>()
-            .await
-            .map_err(|err| HttpError::Unprocessable(format!("{err}")))?;
+        let axum::Json(req) =
+            axum::Json::<<J as FromValidate>::Validatable>::from_request(req, _state)
+                .await
+                .map_err(|err| HttpError::Unprocessable(format!("{err}")))?;
         req.validate().map_err(HttpError::Validation)?;
         Ok(Self(FromValidate::from(req)))
     }
 }
 
 pub trait FromValidate {
-    type Validatable;
+    type Validatable: Validate + for<'de> Deserialize<'de>;
 
     fn from(value: Self::Validatable) -> Self;
 }
@@ -38,11 +39,19 @@ pub trait FromValidate {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use axum::body::Body;
+    use serde::Serialize;
 
-    #[derive(Debug, Clone, Deserialize, Validate)]
+    #[derive(Debug, Clone, Deserialize, Serialize, Validate)]
     struct RawTest {
         #[validate(required)]
         name: Option<String>,
+    }
+
+    impl From<RawTest> for Body {
+        fn from(value: RawTest) -> Body {
+            Body::from(serde_json::to_string(&value).unwrap())
+        }
     }
 
     #[derive(Debug, PartialEq)]
@@ -60,32 +69,17 @@ mod tests {
         }
     }
 
-    impl fmt::Debug for Json<Test> {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-            write!(f, "{:?}", self.0)
-        }
-    }
-
-    #[async_trait]
-    impl FromRequest<(), RawTest> for axum::Json<RawTest> {
-        type Rejection = HttpError;
-
-        async fn from_request(req: Request<RawTest>, _state: &()) -> Result<Self, Self::Rejection> {
-            let body = req.body().clone();
-            Ok(Self(body))
-        }
-    }
-
     #[tokio::test]
     async fn it_gets_raw_request_and_returns_validated_struct() {
-        let body = RawTest {
+        let raw = RawTest {
             name: Some("Tanaka".into()),
         };
 
         let req = Request::builder()
             .method("POST")
+            .header("Content-Type", "application/json")
             .uri("http://foo.bar")
-            .body(body)
+            .body(Body::from(raw))
             .unwrap();
 
         let result = Json::<Test>::from_request(req, &()).await;
@@ -97,18 +91,71 @@ mod tests {
 
     #[tokio::test]
     async fn it_returns_error_if_it_violates_validation() {
-        let body = RawTest { name: None };
+        let raw = RawTest { name: None };
 
         let req = Request::builder()
             .method("POST")
+            .header("Content-Type", "application/json")
             .uri("http://foo.bar")
-            .body(body)
+            .body(Body::from(raw))
+            .unwrap();
+
+        let result = Json::<Test>::from_request(req, &()).await;
+
+        match result {
+            Ok(_) => {
+                unreachable!("The result shoud be an error");
+            }
+            Err(err) => {
+                assert!(matches!(err, HttpError::Validation(_)));
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn it_returns_unprocessable_error_if_it_fails_deserializing() {
+        let req = Request::builder()
+            .method("POST")
+            .header("Content-Type", "application/json")
+            .uri("http://foo.bar")
+            .body(Body::from("{\"foo\":\"bar}"))
             .unwrap();
 
         let result = Json::<Test>::from_request(req, &()).await;
         assert!(result.is_err());
 
-        let err = result.unwrap_err();
-        assert!(matches!(err, HttpError::Validation(_)));
+        match result {
+            Ok(_) => {
+                unreachable!("The result shoud be an error");
+            }
+            Err(err) => {
+                assert!(matches!(err, HttpError::Unprocessable(_)));
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn it_returns_unprocessable_error_if_there_are_no_content_type_header() {
+        let raw = RawTest {
+            name: Some("Tanaka".into()),
+        };
+
+        let req = Request::builder()
+            .method("POST")
+            .uri("http://foo.bar")
+            .body(Body::from(raw))
+            .unwrap();
+
+        let result = Json::<Test>::from_request(req, &()).await;
+        assert!(result.is_err());
+
+        match result {
+            Ok(_) => {
+                unreachable!("The result shoud be an error");
+            }
+            Err(err) => {
+                assert!(matches!(err, HttpError::Unprocessable(_)));
+            }
+        }
     }
 }
